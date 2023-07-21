8d5ce9d5adeb795d4fabe73968f84f150ab3621d
NOOBNUBY <noobnuby77@gmail.com>
2023-07-21 18:46:53 +0900

    Iron_Ore

--- src/main/kotlin/com/noobnuby/plugin/autosmelt/AutoSmelt.kt
+++ src/main/kotlin/com/noobnuby/plugin/autosmelt/AutoSmelt.kt
@@ -1,9 +1,11 @@
 package com.noobnuby.plugin.autosmelt
 
+import com.noobnuby.plugin.autosmelt.event.BlockBreak
 import org.bukkit.plugin.java.JavaPlugin
 
 class AutoSmelt: JavaPlugin() {
     override fun onEnable() {
         logger.info("Enable Plugin")
+        server.pluginManager.registerEvents(BlockBreak(), this)
     }
 }
