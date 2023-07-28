4b1e331abaa1358abcf3fd59b552cb93073a922f
NOOBNUBY <noobnuby77@gmail.com>
2023-07-28 11:27:25 +0900

    TODO : 광물 드롭 안되는거 수정

--- src/main/kotlin/com/noobnuby/plugin/autosmelt/event/OreBreak.kt
+++ src/main/kotlin/com/noobnuby/plugin/autosmelt/event/OreBreak.kt
@@ -8,31 +8,36 @@ import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockDropItemEvent
 import org.bukkit.inventory.ItemStack
+import java.util.*
 
 class OreBreak: Listener {
+    val rand = Random()
     @EventHandler
     fun onBlockBreak(e: BlockBreakEvent) {
         val p = e.player
+        val block = e.block.type
         val hand: ItemStack = e.player.inventory.itemInMainHand
+        var Amount: Int = 1
 
-        if(hand.containsEnchantment(Enchantment.SILK_TOUCH))
-            return
-
+        //TODO : 광물 드롭 안되는거 수정
         if(p.gameMode == GameMode.SURVIVAL) {
-            val block = e.block.type
             when {
+                hand.containsEnchantment(Enchantment.SILK_TOUCH) -> return
+                block != Material.COPPER_ORE || block != Material.DEEPSLATE_COPPER_ORE && hand.containsEnchantment(Enchantment.LOOT_BONUS_BLOCKS) -> Amount = rand.nextInt(hand.getEnchantmentLevel(Enchantment.LOOT_BONUS_BLOCKS) + 1) + 1
                 block == Material.IRON_ORE || block == Material.DEEPSLATE_IRON_ORE -> {
-                    e.isDropItems = false
-                    e.block.world.dropItemNaturally(e.block.location, ItemStack(Material.IRON_INGOT))
-
+                    e.block.world.dropItemNaturally(e.block.location, ItemStack(Material.IRON_INGOT, Amount))
                 }
                 block == Material.GOLD_ORE || block == Material.DEEPSLATE_GOLD_ORE -> {
-                    e.isDropItems = false
-                    e.block.world.dropItemNaturally(e.block.location, ItemStack(Material.GOLD_INGOT))
+                    e.block.world.dropItemNaturally(e.block.location, ItemStack(Material.GOLD_INGOT, Amount))
                 }
                 block == Material.COPPER_ORE || block == Material.DEEPSLATE_COPPER_ORE -> {
-                    e.isDropItems = false
-                    e.block.world.dropItemNaturally(e.block.location, ItemStack(Material.COPPER_INGOT))
+                    if(hand.containsEnchantment(Enchantment.LOOT_BONUS_BLOCKS)) {
+                        Amount = rand.nextInt(6,hand.getEnchantmentLevel(Enchantment.LOOT_BONUS_BLOCKS) + 1 * 5) + 1
+                        e.block.world.dropItemNaturally(e.block.location, ItemStack(Material.COPPER_INGOT, Amount))
+                        return
+                    }
+                    Amount = rand.nextInt(4) + 1
+                    e.block.world.dropItemNaturally(e.block.location, ItemStack(Material.COPPER_INGOT, Amount))
                 }
             }
         }
